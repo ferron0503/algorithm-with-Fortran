092ca107be3febf1f9e6103e6bc4e17ad206fbb5
Ferron503 <62104059+ferron0503@users.noreply.github.com>
2024-10-01 00:31:48 +0900

    Update quicksort_v1.f95

--- quicksort_v1.f95
+++ quicksort_v1.f95
@@ -10,7 +10,7 @@ module quicksort_recursive_v1
             
             if (left >= right) return
             
-            pivot = X((left + right)/2)
+            pivot = median3(X(left), X((left + right)/2), X(right))
             i = left
             j = right
             
@@ -33,4 +33,16 @@ module quicksort_recursive_v1
             call sort(X,left,j)
             call sort(X,i,right)
         end subroutine sort
+        
+        function median3(a, b, c) result(median)
+            real(16), intent(in) :: a, b, c
+            real(16) :: median
+            if ((a < b .and. b < c) .or. (c < b .and. b < a)) then
+                median = b
+            else if ((b < a .and. a < c) .or. (c < a .and. a < b)) then
+                median = a
+            else
+                median = c
+            end if
+        end function median3
 end module quicksort_recursive_v1
